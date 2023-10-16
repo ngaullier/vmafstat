8b9b7ed4ece8afa7fae5c9d1a47b91ee04222fc9
Nicolas Gaullier <nicolas.gaullier@cji.paris>
2023-10-16 14:57:00 +0200

    Renamings, fix --start-main

--- ffsync_cmd.sh
+++ ffsync_cmd.sh
@@ -1,16 +1,16 @@
 usage()
 {
     echo "Usage: $(basename "$0") [-h] [-v] [-q] [-V] -r reference_input -i main_input" 1>&2
-    echo "      [-s start_time] [--start-main start_time_main] [-o max_time_offset] [-d segment_duration]" 1>&2
+    echo "      [-s start_time] [--start_main start_time_main] [-o max_time_offset] [-d segment_duration]" 1>&2
     echo "      [-l confidence_level]" 1>&2
     echo 1>&2
     echo "Determine the alignment between two inputs using psnr maximization." 1>&2
-    echo "The segments to analyze are determined by their start positions (see -s and --start-main)," 1>&2
+    echo "The segments to analyze are determined by their start positions (see -s and --start_main)," 1>&2
     echo "+/- an additionnal offset applied to main_input (see -o)." 1>&2
     echo "If the reference_input fps is greater than that of the main_input (ex: 50p->25p)," 1>&2
     echo "an additionnal test is to try a +1 offset of the reference_input to take frame drops into account." 1>&2
     echo "Frame-psnr values shall fluctuate enough within the duration to make sure alignment is achieved (see -d and -l)." 1>&2
-    echo "If the psnr difference is below the confidence level, the start points will be moved ${trim_retry_shift_s} seconds later for another try," 1>&2
+    echo "If the psnr difference is below the confidence level, the start points will be moved later (see --retry_shift) for another try," 1>&2
     echo "and there is a total of 3 tries before failing definetely." 1>&2
     echo 1>&2
     echo "On success, raw integer parsable values are displayed on stdout: trim_ref, trim_main, psnr_diff, psnr." 1>&2
@@ -24,11 +24,12 @@ usage()
     echo "  -V, --verbose   increase verbosity level (-V -V means debug and is the max verbose level)." 1>&2
     echo "  -r --ref        reference input file." 1>&2
     echo "  -i --main       processed input file." 1>&2
-    echo "  -s --start-ref  position (seconds) in the reference file. Defaults ${trim_ref_base_s}." 1>&2
-    echo "     --start-main position (seconds) in the main file. Defaults=same start as reference file." 1>&2
-    echo "  -o --max-offset max advance/delay (seconds). Defaults ${max_delta_s}." 1>&2
+    echo "  -s --start_ref  position (seconds) in the reference file. Defaults ${start_ref_base_s}." 1>&2
+    echo "     --start_main position (seconds) in the main file. Defaults=same start as reference file." 1>&2
+    echo "  -o --max_offset max advance/delay (seconds). Defaults ${max_offset_s}." 1>&2
     echo "  -d --duration   duration (seconds) of the segment for psnr computation. Defaults ${seg_duration_s}." 1>&2
-    echo "  -l --level      integer value, minimum of (max psnr - min psnr) * 100 to assume successfull sync. Defaults ${min_psnr_diff}." 1>&2
+    echo "  -l --level       integer value, minimum of (max psnr - min psnr) * 100 to assume successfull sync. Defaults ${min_psnr_diff}." 1>&2
+    echo "     --retry_shift after sync failed: shift (seconds) to apply to start times before trying again. Defaults ${retry_start_shift_s}." 1>&2
     echo 1>&2
     echo "Example:" 1>&2
     echo "  readarray sync_info <<< \\" 1>&2
@@ -64,10 +65,10 @@ get_opts()
     verbose=1
     file_ref=
     file_main=
-    trim_ref_base_s=0.2
-    trim_main_base_s=auto
-    trim_retry_shift_s=3
-    max_delta_s=0.12
+    start_ref_base_s=0.12
+    start_main_base_s=auto
+    retry_start_shift_s=3
+    max_offset_s=0.12
     seg_duration_s=1
     min_psnr_diff=800
     while [[ $# -gt 0 ]]
@@ -82,17 +83,17 @@ get_opts()
                 check_arg_value "$key" $#
                 file_main="$2"
                 shift 2 && continue;;
-            -s|--start-ref)
+            -s|--start_ref)
                 check_arg_value "$key" $#
-                trim_ref_base_s="$2"
+                start_ref_base_s="$2"
                 shift 2 && continue;;
-            --start-main)
+            --start_main)
                 check_arg_value "$key" $#
-                trim_main_base_s="$2"
+                start_main_base_s="$2"
                 shift 2 && continue;;
-            -o|--max-offset)
+            -o|--max_offset)
                 check_arg_value "$key" $#
-                max_delta_s="$2"
+                max_offset_s="$2"
                 shift 2 && continue;;
             -d|--duration)
                 check_arg_value "$key" $#
@@ -101,6 +102,10 @@ get_opts()
             -l|--level)
                 check_arg_value "$key" $#
                 min_psnr_diff="$2"
+                shift 2 && continue;;
+            --retry_shift)
+                check_arg_value "$key" $#
+                retry_start_shift_s="$2"
                 shift 2 && continue;;
             -q|--brief)
                 verbose=0
@@ -118,5 +123,5 @@ get_opts()
         echo "$key: unexpected option"
         exit 1
     done
-    [[ $trim_main_base_s = "auto" ]] && trim_main_base_s=$trim_ref_base_s
+    [[ $start_main_base_s != "auto" ]] || start_main_base_s=$start_ref_base_s
 }
