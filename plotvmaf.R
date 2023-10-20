c9fd3695c4fc15ddac48be23cd7f1d01b2ce4435
Nicolas Gaullier <nicolas.gaullier@cji.paris>
2023-10-20 15:47:05 +0200

    plotvmaf: replace optparse with argparse and allow multiple inputs

--- plotvmaf.R
+++ plotvmaf.R
@@ -1,33 +1,28 @@
 #!/usr/bin/env -S Rscript --vanilla
 
-library("optparse")
+library("argparse")
 library("caTools")
 library("rjsonpath")
 msg.trap <- capture.output( suppressMessages( library(plotly) ))
 
 # cmdline
-parser <- OptionParser()
-parser <- add_option(parser, c("-i", "--input"), type="character", default=NULL,
-            help="json input file", metavar="character")
-parser <- add_option(parser, c("-o", "--output"), type="character", default=NULL,
-            help="html output file", metavar="character")
-parser <- add_option(parser, c("-t", "--title"), type="character", default=NULL,
-            help="graph/html title", metavar="character")
-parser <- add_option(parser, c("-s", "--smooth"), type="integer", default=10,
-            help="Number of frames to average for smoothing [default %default]",    metavar="number")
-cmd_options <- parse_args(parser)
+parser <- ArgumentParser(description='Render vmaf json to dynamic (web) graphs')
+parser$add_argument( '-i', '--input',  action='append', help="json input file",  required=TRUE )
+parser$add_argument( '-o', '--output',                  help="html output file", required=TRUE )
+parser$add_argument( '-t', '--title',  type="character",help="graph/html title", default=NULL )
+parser$add_argument( '-s', '--smooth', type="integer", default=10 , help="Number of frames to average for smoothing [default %(default)s]")
 
+args <- parser$parse_args()
 
-in_path=cmd_options[['input']]
-out_path=cmd_options[['output']]
-page_title=cmd_options[['title']]
-runmean_win=cmd_options[['smooth']]
+out_path=args$output
+page_title=args$title
+runmean_win=args$smooth
 
-if (is.null(in_path) || is.null(out_path)) {
-    print_help(parser)
-    q(status=1)
+if (is.null(page_title) ) {
+    page_title=out_path
 }
 
+for (in_path in args$input) {
 # json read / data processing
 json <- read_json(in_path)
 frames = suppressMessages( json_path(json, "$.frames[*].frameNum") )
@@ -48,6 +43,7 @@ print_stats <- list(
     size = 16)
 )
 
+if (!exists("fig")) {
 # Convert frame numbers to timestamps
 # 5 digits are required (%OS5) for proper 2 digits rounding in xaxis/tickformat below
 options("digits.secs"=2)
@@ -55,12 +51,8 @@ frame_time = as.POSIXct(frames/25, origin='1970-01-01', tz='UTC')
 frame_time <- format(frame_time, "%Y-%m-%d %H:%M:%OS5")
 
 # Draw
-dataframe <- data.frame( frame_time, vmaf_frame, vmaf_smooth )
+dataframe <- data.frame( frame_time )
 fig <- plot_ly(dataframe, x = ~frame_time)
-fig <- fig %>% add_trace(y = ~vmaf_frame,  name = 'vmaf_frame',  mode = 'lines', type='scatter', visible='legendonly')
-fig <- fig %>% add_trace(y = ~vmaf_smooth,  name = 'vmaf',  mode = 'lines', type='scatter')
-#fig <- fig %>% add_trace(y = vmaf_var,  name = 'vmaf_var',  mode = 'lines', type='scatter', visible='legendonly')
-
 fig <- fig %>% layout(title = page_title,
          plot_bgcolor='#e5ecf6',
          xaxis = list(
@@ -80,5 +72,9 @@ fig <- fig %>% layout(title = page_title,
          showlegend = TRUE,
          annotations = print_stats
          )
+}
+fig <- fig %>% add_trace(y = vmaf_frame,  name = 'vmaf_frame',  mode = 'lines', type='scatter', visible='legendonly')
+fig <- fig %>% add_trace(y = vmaf_smooth,  name = in_path,  mode = 'lines', type='scatter')
+}
 
 htmlwidgets::saveWidget(fig,out_path,title=page_title,selfcontained=FALSE)
